aebffca465bdc0699c73052a73d1c43a44741418
Dherington1 <dherington1@comcast.net>
2024-03-07 20:32:41 -0800

    login and logout are working on site now

--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -6,10 +6,10 @@ const bcrypt = require('bcryptjs');
 
 // register
 exports.register = catchAsync(async (req, res, next) => {
-    const { name, email, password } = req.body;
-    console.log('username ', name);
+    const { first_name, last_name, email, password } = req.body;
+    console.log('first_name, last_name, ', first_name, last_name,);
 
-    User.create({ name, email, password })
+    User.create({ first_name, last_name, email, password })
     .then(user => {
         // Create JWT
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
@@ -26,32 +26,33 @@ exports.register = catchAsync(async (req, res, next) => {
         
     })
     .catch(err => {
-        if (err.name === 'SequelizeUniqueConstraintError') {
-            const errors = err.errors.map(error => error.message);
-            return res.status(400).json({
-                status: 'fail',
-                message: 'Validation error',
-                errors: errors
-            });
-        } else if (err.name === 'SequelizeValidationError') {
-            const errors = err.errors.map(error => error.message);
-            return res.status(400).json({
-                status: 'fail',
-                message: 'Validation error',
-                errors: errors
-            });
-        } else {
-            return next(err);
-        }
+        // if (err.name === 'SequelizeUniqueConstraintError') {
+        //     const errors = err.errors.map(error => error.message);
+        //     return res.status(400).json({
+        //         status: 'fail',
+        //         message: 'Validation error',
+        //         errors: errors
+        //     });
+        // } else if (err.name === 'SequelizeValidationError') {
+        //     const errors = err.errors.map(error => error.message);
+        //     return res.status(400).json({
+        //         status: 'fail',
+        //         message: 'Validation error',
+        //         errors: errors
+        //     });
+        // } else {
+        //     return next(err);
+        // }
+        console.log(err.message);
     });
 });
 
 // login a user
 exports.login = catchAsync(async (req, res, next) => {
-    const {name, password} = req.body;
+    const {email, password} = req.body;
 
     // Fetch the user
-    const user = await User.findOne({ where: { name } });
+    const user = await User.findOne({ where: { email } });
 
     if (!user) {
         return res.status(404).json({
@@ -75,14 +76,23 @@ exports.login = catchAsync(async (req, res, next) => {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
 
-    // get rid of password when sending back 
+    // Set the token in an HTTP-only cookie
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Send over https in production
+        sameSite: 'strict', // Strictly limit cookie to your site's requests
+        expiresIn: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+    });
+
+    // Get rid of password when sending back the user data
     const userWithoutPassword = {...user.get({ plain: true })};
     delete userWithoutPassword.password;
 
-    // Send status
+    // Send success response without the token in the body
     res.status(200).json({
         status: 'success',
-        token,
         data: {
             user: userWithoutPassword
         }
@@ -90,16 +100,48 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 
+// get user by ID
+exports.getUserById = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
 
+        if (!token) {
+            res.status(401).json({
+                status: 'fail',
+                message: 'you are not logged in'
+            })
+        }
 
+        // Verify and decode the token to extract the user ID or other payload data
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+        // With the decoded ID, you can now fetch the user details
+        // Assuming the decoded token includes the user ID
+        const userId = decoded.id;
+        const user = await User.findById(userId);
 
+        // Return the user information, excluding sensitive data like passwords
+        res.status(200).json({
+            status: 'success',
+            data: {
+                user,
+            },
+        });
 
+    } catch (err) {
+        console.log(err.message);
+    }
+}
 
-
-
-
-
+// logout user
+exports.logoutUser = (req, res) => {
+    res.clearCookie('token', { 
+        httpOnly: true, 
+        secure: process.env.NODE_ENV !== 'development', // Use 'secure' in production
+        sameSite: 'strict',
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
+};
 
 
 
--- Server/Routes/userRoutes.js
+++ Server/Routes/userRoutes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router.get('/allUserData', userController.allUserData)
 router.post('/register', userController.register);
-router.post('/login', userController.login)
+router.post('/login', userController.login);
+router.get('/getUserById', userController.getUserById);
+router.get('/logout', userController.logoutUser);
 
 
 module.exports = router;
